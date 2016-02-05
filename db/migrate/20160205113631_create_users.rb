d4ac3f46d04734eb6f25d8fcabce61129e7b13f6
tobuyo <lllll.skyline.lllll+sub1@gmail.com>
2016-02-05 17:51:08 +0000

    add sign sign up and user detail

--- db/migrate/20160205113631_create_users.rb
+++ db/migrate/20160205113631_create_users.rb
@@ -3,11 +3,11 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :Password_digest
+      t.string :password_digest
 
       t.timestamps null: false
-      
-      t.index :email,unique: true
+
+      t.index :email, unique: true 
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20160205113631) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "Password_digest"
+    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
