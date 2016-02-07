d287d8b774828feefe9a646eac7628ba8563db85
tobuyo <lllll.skyline.lllll+sub1@gmail.com>
2016-02-07 06:02:15 +0000

    daitainn na commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,11 +18,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,:profile,:region)
   end
   
 end
