35ec4eaf98b56c0e3dd8e853f6f16bebe10cb58f
tobuyo <lllll.skyline.lllll+sub1@gmail.com>
2016-02-07 08:55:20 +0000

    ff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,21 +21,29 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    
+    unless current_user==@user
+      redirect_to root_path , notice: '現在のユーザーではありません'
+    end
   end
 
 
   def update
+     @user = User.find(params[:id])
+    unless current_user==@user
+      redirect_to root_path , notice: '現在のユーザーではありません'
+      
+     else
     
-    @user = User.find(params[:id])
-    
-    if @user.update(user_params)
+      if @user.update(user_params)
       # 保存に成功した場合はトップページへリダイレクト
       redirect_to root_path , notice: 'メッセージを編集しました'
-    else
+      else
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
+      end
+     end
     end
-  end
 
   private
 
